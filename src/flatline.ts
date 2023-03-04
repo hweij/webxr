2530698f2e50351edee8c3a02df7807a0d96ee86
hweij <henk000@gmail.com>
2023-03-04 18:36:54 +0100

    fix line for one bend direction other bend is still to be fixed (s > 0)

--- src/flatline.ts
+++ src/flatline.ts
@@ -11,16 +11,27 @@ export function getOutlinePoints(p1: Vector2, p2: Vector2, p3: Vector2, w: numbe
   const nav = new Vector2(av.y, -av.x);
 
   const c = v1.dot(av); // Cosine of the half angle
-  const c2 = Math.sqrt(1 - (c * c));
   const ti = 1 / c;
-  const tj = 1 / c2;
 
-  const inner = (new Vector2()).copy(nav).multiplyScalar(ti * w2).add(p2);
-  const outer = (new Vector2()).copy(nav).multiplyScalar(-w2).add(p2);
-  const pa = (new Vector2()).copy(av).multiplyScalar(-tj * w2).add(p2);
-  const pb = (new Vector2()).copy(av).multiplyScalar(tj * w2).add(p2);
+  const s = v1.dot(n2); // To check bend direction and decide which is inner and which is outer
 
-  console.log(`c = ${c.toFixed(3)} ti = ${ti.toFixed(3)} pInner = (${inner.x.toFixed(3)}, ${inner.y.toFixed(3)}) pOuter = (${outer.x.toFixed(3)}, ${outer.y.toFixed(3)}), pa = (${pa.x.toFixed(3)}, ${pa.y.toFixed(3)}), pb = (${pb.x.toFixed(3)}, ${pb.y.toFixed(3)})`);
+  let inner, outer, pa, pb;
+
+  if (s <= 0) {
+    inner = (new Vector2()).copy(nav).multiplyScalar(ti * w2).add(p2);
+    outer = (new Vector2()).copy(nav).multiplyScalar(-w2).add(p2);
+    pa = (new Vector2()).copy(n1).multiplyScalar(-w2).add(p2);
+    pb = (new Vector2()).copy(n2).multiplyScalar(-w2).add(p2);
+  }
+  else {
+    // Not correct yet, we need to more clearly distinguish the two situations
+    inner = (new Vector2()).copy(nav).multiplyScalar(-w2 * ti).add(p2);
+    outer = (new Vector2()).copy(nav).multiplyScalar(w2).add(p2);
+    pa = (new Vector2()).copy(n1).multiplyScalar(w2).add(p2);
+    pb = (new Vector2()).copy(n2).multiplyScalar(w2).add(p2);
+  }
+
+  console.log(`c = ${c.toFixed(3)} s = ${s.toFixed(3)} ti = ${ti.toFixed(3)} pInner = (${inner.x.toFixed(3)}, ${inner.y.toFixed(3)}) pOuter = (${outer.x.toFixed(3)}, ${outer.y.toFixed(3)}), pa = (${pa.x.toFixed(3)}, ${pa.y.toFixed(3)}), pb = (${pb.x.toFixed(3)}, ${pb.y.toFixed(3)})`);
 
   return { inner, outer, pa, pb };
   // Find the inner crossing
@@ -53,10 +64,13 @@ export function createFlatLine() {
     new Vector2(0.2, 0.7),
     new Vector2(0.3, 0.9),
     new Vector2(0.4, 1.0),
-    new Vector2(0.5, 0.9),
+    new Vector2(0.5, 0.95),
     new Vector2(0.6, 0.7),
     new Vector2(0.7, 0.4),
     new Vector2(0.8, 0.0),
+    new Vector2(0.9, 0.4),
+    new Vector2(1.0, 0.7),
+    new Vector2(1.0, 0.9),
   ];
 
   const geo = new BufferGeometry();
@@ -72,15 +86,15 @@ export function createFlatLine() {
     indices.push(offset + 2, offset + 3, offset );
     if (i > 1) {
       const prev = offset - 4;
-      indices.push(prev + 2, offset + 1, prev + 3);
-      indices.push(prev + 3, offset + 1, offset);
+      indices.push(prev + 2, offset, offset + 3);
+      indices.push(prev + 2, offset + 3, prev + 3);
     }
   }
 
   geo.setAttribute('position', new BufferAttribute(new Float32Array(positions), 3));
   geo.setIndex(indices);
 
-  const mat = new MeshBasicMaterial({color: "#ff0000", wireframe: true, side: DoubleSide});
+  const mat = new MeshBasicMaterial({color: "#ff0000", wireframe: false, side: DoubleSide});
   const mesh = new Mesh(geo, mat);
   return mesh;
 }
