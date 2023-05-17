2c3fb91f881b07d0b3b1e540f0d0b6231e7f386f
hweij <henk000@gmail.com>
2023-05-17 14:53:14 +0200

    add proposal GameObject3D wrapper

---
+++ src/game_object_3d.ts
@@ -0,0 +1,63 @@
+import { Object3D } from "three";
+
+import { GameObject } from "./game_object";
+
+/**
+ * Wraps a (three) 3D object.
+ * 
+ * This class is used to wrap (larger) objects and present a clean and uniform interface.
+ */
+export class GameObject3D implements GameObject {
+    _parent: GameObject3D | null = null;
+    _children: GameObject3D[] = [];
+    _object3D: Object3D | null = null;
+
+    get parent() {
+        return this._parent;
+    }
+    /** Returns a list of children. Do not modify the array directly. */
+    get children() {
+        return this._children;
+    }
+    get object3D() {
+        return this._object3D;
+    }
+
+    addChild(c: GameObject3D) {
+        const cp = c.parent;
+        if (cp && (cp != this)) {
+            // Remove from old parent
+            cp.removeChild(c);
+        }
+        if (cp != this) {
+            this._children.push(c);
+            // Update three
+            if (c._object3D) {
+                this._object3D?.add(c._object3D);
+                c._object3D.userData.gameObject3D = this;
+            }
+        }
+    }
+    removeChild(c: GameObject3D) {
+        if (c.parent == this) {
+            const idx = this._children.indexOf(c);
+            this._children.splice(idx, 1);
+            // update three
+            if (c._object3D) {
+                this._object3D?.remove(c._object3D);
+                delete c._object3D.userData.gameObject3D;
+            }
+            c._parent = null;
+        }
+    }
+
+    // Interface GameObject
+    tick(_dt: number) {
+        // Override this in subclass
+    }
+
+    /** Cleans up resources, if needed. */
+    dispose() {
+        // Override this in subclass
+    }
+}
