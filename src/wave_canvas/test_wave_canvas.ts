0f1faf1a91b2ea0be409d52b66c70046a50f07c8
hweij <henk000@gmail.com>
2023-05-23 11:18:16 +0200

    create realistic wave canvas test

--- src/wave_canvas/test_wave_canvas.ts
+++ src/wave_canvas/test_wave_canvas.ts
@@ -1,16 +1,34 @@
 import { WaveCanvas } from "./wave_canvas";
 
-const startTime = performance.now();
-
 const canvas = document.body.querySelector("canvas");
 if (canvas) {
+    let t = performance.now();
+    let x = 0;
+
     const waveCanvas = new WaveCanvas(canvas, 2, "red");
-    waveCanvas.moveTo(0,0);
-    for (let i=0; i < canvas.width; i += 0.2) {
-        waveCanvas.lineTo(i, Math.sin(i * 0.1) * 50 + 55);
+    waveCanvas.moveTo(x,signalFunction(x));
+
+    let timer = 0;
+
+    const tick = () => {
+        const tNext = performance.now();
+        const dt = tNext - t;
+        t = tNext;
+        x += (dt * 0.04);
+        if (x < canvas.width) {
+            waveCanvas.lineTo(x, signalFunction(x));
+        }
+        else {
+            // Stop timer
+            window.clearInterval(timer);
+            timer = 0;
+        }
     }
+
+    timer = window.setInterval(tick, 16);
 }
 
-var endTime = performance.now()
-
-console.log(`Test execution time: ${(endTime - startTime).toFixed(2)} ms`);
+function signalFunction(x: number) {
+    const v = Math.sin(x * 0.1);
+    return (v * v * v) * 50 + 55;
+}
