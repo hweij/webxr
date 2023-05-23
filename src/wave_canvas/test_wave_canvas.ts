37361cd8a55ad65ac68773a73c38b9f6e8432318
hweij <henk000@gmail.com>
2023-05-23 15:41:52 +0200

    clean up, correct var names

--- src/wave_canvas/test_wave_canvas.ts
+++ src/wave_canvas/test_wave_canvas.ts
@@ -10,26 +10,26 @@ if (canvasList && canvasList.length) {
     for (const c of canvasList) {
         c.style.backgroundColor = "black";
     }
-    let tAbs = performance.now();
-    let t = 0;
+    let tLast = performance.now();
+    let xpos = 0;
 
     const waveCanvasList: WaveCanvas[] = new Array(canvasList.length);
     for (let i=0; i<canvasList.length; i++) {
         waveCanvasList[i] = new WaveCanvas(canvasList[i], lineWidth, colors[i % colors.length]);
-        waveCanvasList[i].moveTo(0,signalFunction(t, i));
+        waveCanvasList[i].moveTo(0,signalFunction(xpos, i));
     }
 
     let timer = 0;
 
     const tick = () => {
         const tNext = performance.now();
-        const dt = tNext - tAbs;
-        tAbs = tNext;
+        const dt = tNext - tLast;
+        tLast = tNext;
         const dx = dt / pixPerSecond;
-        t += dx;
+        xpos += dx;
         for (let i=0; i<waveCanvasList.length; i++) {
             const wc = waveCanvasList[i];
-            wc.putSample(dx, signalFunction(t, i)); 
+            wc.putSample(dx, signalFunction(xpos, i)); 
         }
         // console.log("Ex speed = " + (performance.now() - tNext).toFixed(2));
     }
