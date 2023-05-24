f4cbacfe0a7e733b03a8e2d04e6816d72d492aac
hweij <henk000@gmail.com>
2023-05-24 11:25:15 +0200

    add multiple waves to 3D monitor

--- src/objects/patient_monitor.ts
+++ src/objects/patient_monitor.ts
@@ -7,9 +7,11 @@ const SCREEN_WIDTH = 0.49;
 const SCREEN_HEIGHT = 0.274;
 const BEZEL_WIDTH = 0.02;
 
+const colors = ["red", "green", "blue", "yellow", "white", "cyan", "magenta"];
+
 export class PatientMonitor extends GameObject3D {
     screen: Mesh;
-    _waveCanvas: WaveCanvas;
+    _waveCanvas: WaveCanvas[];
     _t = 0;
     _tex: Texture;
 
@@ -20,8 +22,14 @@ export class PatientMonitor extends GameObject3D {
         const canvas = document.createElement("canvas");
         canvas.width = 640;
         canvas.height = 360;
-        this._waveCanvas = new WaveCanvas(canvas, {offsetX: 20, offsetY: 20, width: 600, height: 80, color: "red", gapWidth: 20, lineWidth: 2, pixPerSecond: 40});
-        this._tex =  new Texture(this._waveCanvas.canvas);
+        this._tex =  new Texture(canvas);
+        this._waveCanvas = new Array(4);
+        for (let i=0; i<4; i++) {
+            this._waveCanvas[i] = new WaveCanvas(canvas, {
+                offsetX: 20, offsetY: i * 80 + 20, width: 600, height: 80,
+                color: colors[i % colors.length],
+                gapWidth: 20, lineWidth: 2, pixPerSecond: 40});
+        }
 
         {   // Screen
             const geo = new PlaneGeometry(SCREEN_WIDTH, SCREEN_HEIGHT, 1, 1);
@@ -40,15 +48,17 @@ export class PatientMonitor extends GameObject3D {
     override tick(dt: number): void {
         super.tick(dt);
         this._t += dt;
-        if (this._waveCanvas.putSample(this._t, signalFunction(this._t))) {
-            this._tex.needsUpdate = true;
+        for (let i=0; i<this._waveCanvas.length; i++) {
+            if (this._waveCanvas[i].putSample(this._t, signalFunction(this._t, i))) {
+                this._tex.needsUpdate = true;
+            }
         }
     }
 }
 
-function signalFunction(t: number) {
+function signalFunction(t: number, row: number) {
     const amp = 30;
     const mid = 40;
-    const v = Math.sin(t * 2);
+    const v = Math.sin(t * 2 + row);
     return (v * v * v) * amp + mid;
 }
