da596fe655baf8caf0d59d321269e233d0157b60
hweij <henk000@gmail.com>
2023-07-21 07:56:57 +0200

    remove ssl, use port forwarding instead

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 443,
-    https: true,
+    port: 8080,
+    // port: 443,
+    // https: true,
     // Uncomment to allow access from network
     // (or use `npm run dev -- -- host=0.0.0.0`)
     host: '0.0.0.0',
   },
-  plugins: [ basicSsl() ]
+  // plugins: [ basicSsl() ]
 })
