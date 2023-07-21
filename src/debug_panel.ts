33f8f35df16f0e059ff825d3cca7f3e811b709d3
hweij <henk000@gmail.com>
2023-07-21 09:46:32 +0200

    add background and color options to debug panel

--- src/debug_panel.ts
+++ src/debug_panel.ts
@@ -1,16 +1,25 @@
 import * as THREE from "three";
 import { Object3D, Texture } from "three";
 
+export interface PanelOptions {
+  textColor?: string;
+  backgroundColor?: string;
+}
+
 /** Panel for showing in-scene debug messages */
 export class DebugPanel {
   object3D: THREE.Object3D;
   canvas: HTMLCanvasElement;
   tex: Texture;
+  textColor: string;
+  background?: string;
 
-  constructor(parent: Object3D, width: number, height: number) {
+  constructor(parent: Object3D, width: number, height: number, options?: PanelOptions) {
     this.canvas = document.createElement('canvas');
     this.canvas.width = width;
     this.canvas.height = height;
+    this.textColor = options?.textColor || "#ffffff";
+    this.background = options?.backgroundColor;
     this.tex =  new Texture(this.canvas);
     const material = new THREE.SpriteMaterial({ map: this.tex, depthTest: false });
     const sprite = new THREE.Sprite( material );
@@ -25,7 +34,15 @@ export class DebugPanel {
     const ctx = this.canvas.getContext('2d');
     if (ctx) {
       ctx.font = "10px roboto";
-      ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+      if (this.background) {
+        ctx.globalCompositeOperation = "copy";
+        ctx.fillStyle = "#00000033";
+        ctx.fillRect(0,0,this.canvas.width, this.canvas.height);
+        ctx.globalCompositeOperation = "source-over";  
+      }
+      else {
+        ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+      }
       ctx.fillStyle = color;
       for (let i=0; i<lines.length; i++) {
         ctx.fillText(lines[i], 4, 10 * i + 10);
